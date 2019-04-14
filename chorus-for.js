fdd83ed5a4fed496474d3ad8143cc124cda0ee75
Kate <khummle@gmail.com>
2019-04-14 22:28:13 +0000

    Adds looping exercises

---
+++ chorus-for.js
@@ -0,0 +1,11 @@
+var chorus = "Let's dance!";
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
+
+/*a for loop require 3 statement separated by a semicolon. The first
+is  is to initialize the loop (to declare our counter variable),
+the second is to define our WHILE condition, and lastly for updating
+the state of our loop after every step.*/
+
---
+++ packing-list.js
@@ -0,0 +1,12 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+/*this code works because we're delaring a counter variables called
+i (short for index) that starts at 0 and increments by 1 at every step
+until it matches the length of the array*/
+
+packingList[3];

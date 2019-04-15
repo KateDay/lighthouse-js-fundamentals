2cd6a9585624dac0f161185c349f536e83888353
Kate <khummle@gmail.com>
2019-04-15 16:53:36 +0000

    Updating more js practice and assignments

---
+++ concactenate-arrays-assignment.js
@@ -0,0 +1,14 @@
+function concat(z, y) {
+  for (var i = 0; i < y.length; i++) {
+    z.push(y[i]);
+  }
+
+  return z;
+
+}
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
---
+++ looping-Last-index-of-assignment.js
@@ -0,0 +1,18 @@
+function lastIndexOf (array, value) {
+  var i = 0;
+
+  for (var array.length; i >= 0; i--) {
+    if (array[i] === value) {
+      return i;
+
+    } else {
+      return -1; }
+  }
+}
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);

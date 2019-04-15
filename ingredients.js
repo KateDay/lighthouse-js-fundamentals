517a483bac6b1ceab8ca99630f0991aae2f47e8b
Kate <khummle@gmail.com>
2019-04-15 00:22:53 +0000

    saving looping assignments

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+var i = 0
+while (i <= 0) {
+  console.log(ingredients[i]);
+  ingredients =  ingredients++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (var i = 0; i < ingredients.length; i++) {
+  ingredients[i]
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (var i = 0; i < ingredients.length; i--) {
+  ingredients[i]
+}
